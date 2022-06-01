9bffa3a2ce62d935cef1fe4e095b379e6cc8063c
nishant <nishantshah1729@gmail.com>
2022-06-01 13:38:17 +0530

    Js Configured

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+const btn = document.querySelector('.btn-mobile-nav')
+const header = document.querySelector('.header')
 
+btn.addEventListener('click', () => {
+  header.classList.toggle('nav-open')
+})
 
