6f5df5a3947dd9d3264aababf79330ddf4d2d8c2
nishant <nishantshah1729@gmail.com>
2022-06-01 18:01:49 +0530

    Created JS File

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,5 @@ const body = document.querySelector('body')
 btn.addEventListener('click', () => {
   header.classList.toggle('nav-open')
 })
-header.addEventListener('click', () => {
-  if (header.classList.contains('nav-open')) {
-    header.classList.remove('nav-open')
-  }
-})
+
 
