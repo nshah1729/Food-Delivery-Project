033dd926c16d27827f3c3328b997e59a3778e844
nishant <nishantshah1729@gmail.com>
2022-06-01 17:55:52 +0530

    Created JS File

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector('.btn-mobile-nav')
 const header = document.querySelector('.header')
+const body = document.querySelector('body')
 
 btn.addEventListener('click', () => {
+  header.classList.toggle('nav-open')
+})
+body.addEventListener('click', () => {
   header.classList.toggle('nav-open')
 })
 
