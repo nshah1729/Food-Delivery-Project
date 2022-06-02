c389e3959827d64349be94a27071a8390b7f39ab
nishant <nishantshah1729@gmail.com>
2022-06-02 09:36:14 +0530

    Sticky Navbar

--- JS/script.js
+++ JS/script.js
@@ -13,3 +13,20 @@ const yearEl = document.querySelector('.year')
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+////////////////////////////////////////////
+//Sticky Navigation
+const sectionHero = document.querySelector('.section-hero')
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  if (!ent.isIntersecting) {
+    document.body.classList.add('sticky');
+  }
+  else {
+    document.body.classList.remove('sticky');
+  }
+}, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px"
+});
+obs.observe(sectionHero);
